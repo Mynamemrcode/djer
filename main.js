14bcfb6419e36d62a9025794addd0cf9253396f2
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-08-29 16:49:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+music1 = "";
+music2 = "";
+function preload() {
+music1 = loadSound("FearisUsless.mp3");
+music2 = loadSound("FearisUsless.mp3");
+} 
+function setup() {
+    canvas = createCanvas(800,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+} 
+function draw() {
+    image(video,0,0, 800,500);
+}
