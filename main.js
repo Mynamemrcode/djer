6d40e09aa2631d72de37bcce089f20f2b918b0a2
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-09-09 18:09:45 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 music1 = "";
 music2 = "";
+
+leftWristx = "";
+leftWristy = "";
+rightWristx = "";
+rightWristy = "";
 function preload() {
-music1 = loadSound("HappiestMemory.wav");
-music2 = loadSound("Nerve-ATripToPhoenix(1).mp3.mp3");
-} 
+    music1 = loadSound("song.mp3");
+    music2 = loadSound("NerveATripToPhoenix.mp3");
+    }
 function setup() {
     canvas = createCanvas(800,500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poses = ml5.poseNet(video, Loaded);
+    poses.on('pose', Gotposes);
 } 
 function draw() {
     image(video,0,0, 800,500);
+}
+function Gotposes(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristx = results[0].length.pose.leftWrist.x;
+        leftWristy = results[0].length.pose.leftWrist.y;
+        rightWristx = results[0].length.pose.rightWrist.x;
+        rightWristy = results[0].length.pose.rightWrist.y;
+    }
+    
+}
+
+function Loaded() {
+    console.log("loaded");
 }
