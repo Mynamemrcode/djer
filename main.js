d00efbb04800d21a16a0e61673ed304a04d6c1ff
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-09-19 12:15:35 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 music1 = "";
 music2 = "";
 
+leftWristscore = "";
+
 leftWristx = "";
 leftWristy = "";
 rightWristx = "";
 rightWristy = "";
+
+leftsongstat = "";
 function preload() {
     music1 = loadSound("song.mp3");
     music2 = loadSound("NerveATripToPhoenix.mp3");
@@ -19,14 +23,28 @@ function setup() {
 } 
 function draw() {
     image(video,0,0, 800,500);
+    fill("#b8860d");
+    stroke("#b8860d");
+
+    leftsongstat = music1.isPlaying();
+    if(leftWristscore > 0.5) {
+        circle(leftWristx, leftWristy, 40);
+        music2.stop();
+
+        if(leftsongstat == false) {
+            music1.play();
+            document.getElementById("holder").innerHTML = "Song 1 is playing";
+        }
+    }
 }
 function Gotposes(results) {
     if(results.length > 0) {
         console.log(results);
-        leftWristx = results[0].length.pose.leftWrist.x;
-        leftWristy = results[0].length.pose.leftWrist.y;
-        rightWristx = results[0].length.pose.rightWrist.x;
-        rightWristy = results[0].length.pose.rightWrist.y;
+        leftWristx = results[0].pose.leftWrist.x;
+        leftWristy = results[0].pose.leftWrist.y;
+        rightWristx = results[0].pose.rightWrist.x;
+        rightWristy = results[0].pose.rightWrist.y;
+        leftWristscore = results[0].pose.keypoints[9].score;
     }
     
 }
@@ -34,3 +52,5 @@ function Gotposes(results) {
 function Loaded() {
     console.log("loaded");
 }
+
+
